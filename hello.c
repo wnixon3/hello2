5f27a1b7acb782e2a934413ea08bbdcb20585e86
Will Nixon <wnixon3@huskers.unl.edu>
2020-08-21 18:49:58 +0000

    added major

--- hello.c
+++ hello.c
@@ -1,13 +1,16 @@
 /**
  * Author: Will Nixon
  * Date: 2020/08/21
- * 
+ *
  * Simple hello.c
  */
- #include <stdlib.h> 
+ #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
 
- printf("William Nixon\n")
+ printf("William Nixon!\n");
+ printf("Computer Engineering\n");
+
+ return 0
  }

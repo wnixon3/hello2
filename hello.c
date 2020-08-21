5ff3da864d4bb3557cd54f0ce8b91e1601a03fe3
Will Nixon <wnixon3@huskers.unl.edu>
2020-08-21 18:33:13 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Will Nixon
+ * Date: 2020/08/21
+ * 
+ * Simple hello.c
+ */
+ #include <stdlib.h> 
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+
+ printf("William Nixon\n")
+ }
